5ab366236cec685c7c3f3cef421e731689a3ae85
Gurpreet <gurpreet.khanuja01@gmail.com>
2019-08-28 16:00:09 -0400

    Code improvement in UserDAO.java

--- src/main/java/com/dao/UserDAO.java
+++ src/main/java/com/dao/UserDAO.java
@@ -11,12 +11,26 @@ import java.util.List;
 
 public class UserDAO {
 
+	static Connection conn = null;
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/LoginDb", "root", "root");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+	}
+	
     public int registerUser(Credentials credentials) {
 
         String query = "insert into loign(username, password) values (?, ?)";
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/LoginDb", "root", "root");
+            
             PreparedStatement preparedStatement = conn.prepareStatement(query);
             {
                 preparedStatement.setString(1, credentials.getUserName());
@@ -26,8 +40,6 @@ public class UserDAO {
 
                 return row;
             }
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -40,10 +52,8 @@ public class UserDAO {
                 "values( ?, ?, ?)";
 		int count  = 0;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/LoginDb", "root", "root");
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+                
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
 
             preparedStatement.setInt(1, registerUser.getUniversityNumber());
             preparedStatement.setString(2, registerUser.getStudentName());
@@ -51,8 +61,6 @@ public class UserDAO {
             count = preparedStatement.executeUpdate();
            
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (SQLException e) {
             //logging : log the root cause
         	throw new DuplicateIdException("Id" + registerUser.getUniversityNumber() + " is duplicate !!");
@@ -66,9 +74,8 @@ public class UserDAO {
 
        List<User> regList = new LinkedList<>();
         try{
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/LoginDb", "root", "root");
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+         
+            PreparedStatement preparedStatement = conn.prepareStatement(query);
             ResultSet registeredStudentList = preparedStatement.executeQuery();
              while (registeredStudentList.next()){
 
@@ -81,9 +88,7 @@ public class UserDAO {
 
              }
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
+        }  catch (SQLException e) {
             e.printStackTrace();
         }
 
